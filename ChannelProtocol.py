39b01383aca418abd3db6baac19105a025536a21
Rishat Valitov <valitov.rishat@gmail.com>
2019-11-20 19:02:09 +0300

    [BEGIN] Added main classes

---
+++ ChannelProtocol.py
@@ -0,0 +1,6 @@
+class ChannelProtocol(object):
+    def __init__(self):
+        self.start = 0x2700
+        self.msg_type = 0
+        self.addr = 0
+        self.phy = 0
---
+++ Node.py
@@ -0,0 +1,6 @@
+class Node(object):
+    def __init__(self):
+        self.address = 0
+        self.connections = []
+        self.alive = []
+        self.history = []
